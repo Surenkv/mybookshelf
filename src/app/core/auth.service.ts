46621a871da9ef14ac4d6717b0e1b8bed944e01b
Suren <Surenvijayaragavan@gmail.com>
2018-06-10 20:15:52 +0530

    completed

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -3,6 +3,7 @@ import { AngularFireAuth } from 'angularfire2/auth'
 import * as firebase from 'firebase/app'
 import { Observable } from 'rxjs';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,7 @@ export class AuthService {
   authState: any = null;
   userState:any=null;
   newUserID:any=null;
-  constructor(private afs: AngularFirestore,public afAuth: AngularFireAuth) {
+  constructor(private afs: AngularFirestore,public afAuth: AngularFireAuth, private router:Router) {
     this.afAuth.authState.subscribe(data => this.authState = data)
   }
 
@@ -29,10 +30,12 @@ export class AuthService {
       let newUser={"userName":this.userState.user.displayName,"imageUrl":this.userState.user.photoURL,"logId":this.userState.user.uid};
       this.newUserID=this.userState.user.uid;
       this.afs.collection('bookshelf').doc(this.newUserID).set(newUser);
+      this.router.navigate(['library']);
     });
 
   }
   logout() {
     this.afAuth.auth.signOut()
+    alert("You have Successfully Logged Out . .")
   }
 }
